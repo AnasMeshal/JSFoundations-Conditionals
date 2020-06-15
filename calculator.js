68ea0b938706a9cfe68fa762b5f63cb8821d4a94
AnasMeshal <Fire-Ball-KwT@hotmail.com>
2020-06-15 20:11:02 +0300

    task 2 done

--- calculator.js
+++ calculator.js
@@ -1,3 +1,12 @@
 // Define the numbers and the operation here
-
+const num1 = 10; const num2 = 5;  const operation = "*";
 // Write if-statements here
+if (operation === "+") {
+    console.log(`${num1} + ${num2} = ${num1 + num2}`);
+} else if (operation === "-") {
+    console.log(`${num1} - ${num2} = ${num1 - num2}`);
+} else if (operation === "*") {
+    console.log(`${num1} * ${num2} = ${num1 * num2}`);
+} else {
+    console.log(`${num1} / ${num2} = ${num1 / num2}`);
+}
